908b28faf61aea19ad38ec56f050b10fca06a545
Yuankai Yue <Yuankai Yue>
2016-09-27 16:50:57 -0500

    update working version of BFS

--- Main.java
+++ Main.java
@@ -37,8 +37,8 @@ public class Main {
 			ps = System.out;			// default to Stdout
 		}
 		initialize();
-		printLadder(getWordLadderBFS("smart", "money"));
-		//System.out.println(isNeighbor("toney","toney"));
+		printLadder(getWordLadderBFS("books", "money"));
+		//System.out.println(getNeighbors("honey"));
 		// TODO methods to read in words, output ladder
 	}
 	
@@ -74,12 +74,14 @@ public class Main {
 		ArrayList<String> neighbors;
 		Iterator<String> itr;
 		String current,next;
+		boolean ladderFound=false;
 		q.add(start);
+		visited.add(start);
 		while(!q.isEmpty()){
 			current=(String)q.poll();
-			visited.add(current);
 			if(current.equals(end)){
-				connect.put(end, current);
+				ladderFound=true;
+				//connect.put(end, current);
 				break;
 			}
 			neighbors = getNeighbors(current);
@@ -87,12 +89,19 @@ public class Main {
 			while(itr.hasNext()){
 				next = (String)itr.next();
 				if(!visited.contains(next)){
+					visited.add(next);
 					q.add(next);
 					connect.put(next, current);
 				}
 			}
 		}
-		return getLadder(connect,end); // replace this line later with real return
+		if(ladderFound)
+			return getLadder(connect,end);
+		else{
+			System.out.println("no word ladder can be found between " + start+" and "+end+".");
+			return null;
+		}
+			
 	}
     /**
      * Returns a list of neighbors of word. Neighbors different from word by only 1 letter.
@@ -100,7 +109,6 @@ public class Main {
      * @return	a list of neighbors.
      */
     private static ArrayList<String> getNeighbors(String word){
-    	Set<String> dict = makeDictionary();
     	ArrayList<String> neighbors = new ArrayList<String>();
     	Iterator itr= dict.iterator();
     	String next;
@@ -127,14 +135,39 @@ public class Main {
     	return counter==1;
     }
     
+    /**
+     * Construct a ladder from a map, the key and value are neighboring words.
+     * @param map with neighboring words.
+     * @param endWord the end word.
+     * @return ArrayList with of the word ladder
+     */
     private static ArrayList<String> getLadder(HashMap<String, String> map, String endWord){
     	 ArrayList<String> ladder = new  ArrayList<String>();
     	 String key = endWord;
+    	 String next;
     	 ladder.add(key);
     	 while(map.containsKey(key)){
-    		 ladder.add(map.get(key));
+    		 next = map.remove(key);
+    		 ladder.add(next);
+    		 key=next;
     	 }
-    	return ladder;
+
+    	return reverse(ladder);
+    }
+    
+    /**
+     * Reverse a arraylist
+     * @param list to be reversed
+     * @return reversed list
+     */
+    
+    private static ArrayList<String> reverse(ArrayList<String> list){
+	   	 ArrayList<String> reverse= new ArrayList<String>();
+	   	 int size = list.size();
+	   	 for(int i =0; i<size;i++){
+	   		 reverse.add(list.get(size-1-i));
+	   	 }
+	   	 return reverse;
     }
     
 	public static Set<String>  makeDictionary () {
@@ -161,16 +194,11 @@ public class Main {
 	
 	public static void printLadder(ArrayList<String> ladder){
 		int ladderHeight = ladder.size();
-		System.out.println(ladder.toString());
-		if(ladderHeight <2){
-			System.out.println("Invalid ladder");
-			return;
-		}
-		if(ladderHeight==2){
-			System.out.println("no word ladder can be found between <start> and <finish>.");
-		}else{
-			System.out.println("a 8-rung word ladder exists between smart and money.");
-			for(int i=1; i<ladderHeight-1; i++){
+		if(ladderHeight<2)
+			throw new IllegalArgumentException("Illegal ladder arraylist input, ladder length shuold be larger than 1.");
+		else{
+			System.out.println("a "+(ladderHeight-2)+"-rung word ladder exists between "+ladder.get(0)+" and "+ladder.get(ladder.size()-1)+" .");
+			for(int i=0; i<ladderHeight; i++){
 				System.out.println(ladder.get(i));
 			}
 		}
