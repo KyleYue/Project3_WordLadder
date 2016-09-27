5cafe090186c8e7e7b76ebda7309faa40ee069bd
Eric Liang <jklelxy@gmail.com>
2016-09-27 14:07:44 -0500

    added unit test file (#1)
    Added a unit test file.

---
+++ UnitTest.java
@@ -0,0 +1,29 @@
+/* WORD LADDER UnitTest.java
+ * EE422C Project 3 submission by
+ * Replace <...> with your actual data.
+ * Xiaoyong Liang
+ * XL5432
+ * 16480
+ * Yuankai Yue
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Slip days used: <0>
+ * Git URL:
+ * Fall 2016
+ */
+package assignment3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Eric on 9/27/16.
+ */
+public class UnitTest {
+
+    @Test
+    public void testParse() {
+        
+    }
+}
